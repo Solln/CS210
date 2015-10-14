1af78f8be3cc00b7134ab27d07b827d69e6cce90
Darren <Darren-Lowe@Live.co.uk>
2015-10-14 20:46:44 +0100

    Add_Variables_And_Calcs

--- P0v1.c
+++ P0v1.c
@@ -1,6 +1,6 @@
 /**************************************************************************
  *
- * Filename: P0v1.c
+ * Filename: P1v1.c
  *
  * Synopsis: Practical 1
  * 
@@ -19,29 +19,32 @@
  **************************************************************************/
 
 
-#define VERSION "p0 v1.0 by Duncan. Last update: 05/10/2014\n\n"
+#define VERSION "p1 v1.0 by Darren Lowe. Last update: 10/10/2015\n\n"
 
-#include <stdio.h>	/* Include standard library functions for I/O */
+#include <stdio.h>
 
 int main() {
 /* Add suitable variables here as part of Practial 1 */
-	printf(VERSION);	/* Might as well know who wrote this thing! */
+
+
+float executeTimeOnX = 10, executeTimeOnY = 15;
+
+float cpuTimeOnX = 10, cpuTimeOnY = 6;
+
+float clockSpeedOnX = 2.00, clockSpeedOnY = ((1.2*clockSpeedOnX)*10)/cpuTimeOnY;
+
+	printf(VERSION);	
+
+printf("%fs on X, %fs on Y. So X is %f times faster than Y\n\n", executeTimeOnX, executeTimeOnY, (executeTimeOnY/executeTimeOnX));
+
+printf("X: %fGHz clock, %fs CPU time; Y: ?GHz clock, %fs CPU time. \n\nTherefore clock rate of Y needs to be %fGHz\n\n", clockSpeedOnX, cpuTimeOnX, cpuTimeOnY, clockSpeedOnY);
 
 /* The following code is just a very simple template.
    For Practical 1, the hard-wired constants in the printf()s will need to
    be replaced by variables & (the results of) the performance calculations.
    See [CS210_01_hw/18,21-24] for the equations.
  */
-	/* Example from CS210_01_hw/18 */
-	printf("10s on A, 15s on B. So A is 1.5 times faster than B\n\n");
-	/* Example from CS210_01_hw/22 */
-	printf("A: 2GHz clock, 10s CPU time; B: ?GHz clock, 6s CPU time\n");
-	printf("\tTherefore clock rate of B needs to be 4GHz\n\n");
-	/* Example from CS210_01_hw/24 */
-	printf("A: Cycle time=250ps, CPI=2.0; B: Cycle time=500ps, CPI=1.2\n");
-	printf("\tSo, CPU Time_A is IC x CPI_A x Cycle Time_A => IC x 500ps\n");
-	printf("\tSo, CPU Time_B is IC x CPI_B x Cycle Time_B => IC x 600ps\n");
-	printf("\t\tTherefore A is 1.2 times faster than B\n");
+
 	
 	return 0;
 }
