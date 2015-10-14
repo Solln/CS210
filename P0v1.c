6142e9af197ef5100ecdd5cad63795125a0ba04a
Darren <Darren-Lowe@Live.co.uk>
2015-10-14 23:45:39 +0100

    Finished_Calculations

--- P0v1.c
+++ P0v1.c
@@ -33,17 +33,18 @@ float cpuTimeOnX = 10, cpuTimeOnY = 6;
 
 float clockSpeedOnX = 2.00, clockSpeedOnY = ((1.2*clockSpeedOnX)*10)/cpuTimeOnY;
 
-	printf(VERSION);	
+float cycleTimeOnX = 250, cycleTimeOnY = 500;
+
+float cpiOnX = 2, cpiOnY = 1.2;
+
+printf(VERSION);	
 
 printf("%fs on X, %fs on Y. So X is %f times faster than Y\n\n", executeTimeOnX, executeTimeOnY, (executeTimeOnY/executeTimeOnX));
 
-printf("X: %fGHz clock, %fs CPU time; Y: ?GHz clock, %fs CPU time. \n\nTherefore clock rate of Y needs to be %fGHz\n\n", clockSpeedOnX, cpuTimeOnX, cpuTimeOnY, clockSpeedOnY);
+printf("X: %fGHz clock, %fs CPU time; Y: ?GHz clock, %fs CPU time. \n    Therefore clock rate of Y needs to be %fGHz\n\n", clockSpeedOnX, cpuTimeOnX, cpuTimeOnY, clockSpeedOnY);
 
-/* The following code is just a very simple template.
-   For Practical 1, the hard-wired constants in the printf()s will need to
-   be replaced by variables & (the results of) the performance calculations.
-   See [CS210_01_hw/18,21-24] for the equations.
- */
+printf("X: Cycle time=%fps, CPI=%f; Y: Cycle time=%fps, CPI=%f \n    So, CPU Time_X is IC x CPI_X x Cycle Time_X => IC x %fps \n", cycleTimeOnX, cpiOnX, cycleTimeOnY, cpiOnY, (cycleTimeOnX*cpiOnX));
+printf("    So, CPU Time_Y is IC x CPI_Y x Cycle Time_Y => IC x %f \n        Therefore X is %f times faster than Y\n\n", (cycleTimeOnY*cpiOnY), (cycleTimeOnY*cpiOnY)/(cycleTimeOnX*cpiOnX));
 
 	
 	return 0;
